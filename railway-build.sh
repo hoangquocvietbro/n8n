2cd90e742ffa5a3d68d57f87e4f99d9873331db1
Hoàng Quốc Việt <74923175+hoangquocvietbro@users.noreply.github.com>
2025-01-06 11:04:47 +0700

    Update railway-build.sh

--- railway-build.sh
+++ railway-build.sh
@@ -6,7 +6,7 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  curl -o google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
 else
